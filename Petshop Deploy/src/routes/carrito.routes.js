04d81730fa4afdf1502acbb9541e0e9b2406769f
Salvador <lairihoysalvadorr@gmail.com>
2025-11-21 21:02:30 -0300

    Delete Petshop Deploy directory

--- Petshop Deploy/src/routes/carrito.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { crearPedido } = require("../controllers/carrito.controller");
-
-router.post("/", crearPedido);
-
-module.exports = router;
